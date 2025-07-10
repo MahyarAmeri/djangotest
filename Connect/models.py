17025709bc8c055633ecf145b3b63c6c7e36ec39
MahyarAmeri <mahyar82ama@gmail.com>
2025-07-10 16:32:04 +0430

    test10

--- Connect/models.py
+++ Connect/models.py
@@ -9,3 +9,7 @@ class massage(models.Model):
     receiver = models.ForeignKey(User,on_delete=models.CASCADE, related_name='receiver')
     message = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+
+
+class CountView(models.Model):
+    count = models.IntegerField(default=0)
