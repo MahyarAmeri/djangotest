151740fabce37220b52ab1dad5d836bf9a3cfbf2
MahyarAmeri <mahyar82ama@gmail.com>
2025-07-12 14:16:09 +0430

    test16

--- Connect/admin.py
+++ Connect/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import massage, CountView
+from .models import massage, CountView, DoorClick
 # Register your models here.
 
 admin.site.register(massage)
 admin.site.register(CountView)
+admin.site.register(DoorClick)
